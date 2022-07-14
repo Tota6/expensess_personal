bf9f300dfd5a1423f8f4f371acc702d62eb1b903
Martina Kamal <tt034232@gmail.com>
2022-07-14 17:10:31 +0200

    adding a scrolling view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import './transactions.dart';
+import 'package:expensses_planner/widgets/user_tansaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -15,74 +15,31 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  List<Transactions> transactions = [
-    Transactions(
-      id: "t1",
-      title: "new shoes",
-      amount: 225,
-      date: DateTime.now(),
-    ),
-    Transactions(
-      id: "t2",
-      title: "new nail polish",
-      amount: 16.99,
-      date: DateTime.now(),
-    ),
-  ];
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expention personal"),
       ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Card(
-                color: Color.fromARGB(255, 235, 216, 159),
-                elevation: 5,
-                child: Container(width: double.infinity, child: Text("chart")),
+      body: SingleChildScrollView(
+        child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                child: Card(
+                  color: const Color.fromARGB(255, 235, 216, 159),
+                  elevation: 5,
+                  child: Container(
+                      width: double.infinity, child: const Text("chart")),
+                ),
               ),
-            ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 25),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: const Color.fromARGB(255, 243, 108, 59),
-                                width: 2)),
-                        child: Text("LE ${tx.amount.toString()}",
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Color.fromARGB(255, 243, 108, 59))),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(tx.title,
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold)),
-                          Text(
-                            tx.date.toString(),
-                            style: const TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            ),
-          ]),
+              UserTarnsaction()
+            ]),
+      ),
     );
   }
 }
